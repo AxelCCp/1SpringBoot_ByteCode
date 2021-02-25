665b9ecb291fb8d04725e4268b6e6dfbb484142a
Boo <hpmajin@outlook.es>
2021-02-24 23:31:43 -0600

    CLASE 6:Proyecto Spring Boot configurado V.1

---
+++ A0_CursoSpringBoot/src/main/java/com/bytecode/core/A0CursoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.bytecode.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A0CursoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A0CursoSpringBootApplication.class, args);
+	}
+
+}
